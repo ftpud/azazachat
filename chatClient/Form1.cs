f843b9f239c944c891f1e959a1fd1c4beed25e4d
ftpud <bober12@inbox.lv>
2014-09-25 21:54:50 +0300

    initial

---
+++ chatClient/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net.Sockets;
+using System.Threading;
+
+namespace chatClient
+{
+    public partial class Form1 : Form
+    {
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        chatClient chat;
+        System.Windows.Forms.Timer bgworker = new System.Windows.Forms.Timer();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chat = new chatClient();
+            bgworker.Interval = 100;
+            bgworker.Start();
+            bgworker.Tick += bgworker_Tick;
+                        
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            chat.Stop();
+            base.OnClosing(e);
+        }
+
+        void bgworker_Tick(object sender, EventArgs e)
+        {
+            string s = chat.getMessage();
+            if (s != "") { listBox1.Items.Insert(0, s); }
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            chat.sendMessage(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void Form1_Leave(object sender, EventArgs e)
+        {
+         
+        }
+    }
+
+    // chat client class
+    public class chatClient
+    {
+        int port = 12312;
+        TcpClient client = new TcpClient();
+
+
+        List<string> Msgs = new List<string>();
+        List<string> SendMessages = new List<string>();
+
+        public void Stop()
+        { cThread.Abort(); }
+
+        public string getMessage()
+        {
+            if (Msgs.Count > 0)
+            { 
+                lock(Msgs)
+                {
+                    string s = Msgs[0];
+                    Msgs.RemoveAt(0);
+                    return s;
+                }
+            }
+            return "";
+        }
+
+        public void sendMessage(string str)
+        {
+            SendMessages.Add(str);
+        }
+
+        Thread cThread;
+        public chatClient()
+        {
+            client.Connect("localhost", port);
+            if (client.Connected)
+            {
+                 cThread = new Thread(new ParameterizedThreadStart(chatThread));
+                cThread.Start(client);
+            }
+        }
+
+        void chatThread(object client)
+        {
+            TcpClient Client = (TcpClient)client;
+            NetworkStream ns = Client.GetStream();
+
+            while (Client.Connected)
+            {
+                try {           
+                    // read
+                    if(ns.DataAvailable)
+                    {
+                        byte[] buf = new byte [4096];
+                        int read = ns.Read(buf, 0, 4096);
+                        string input = UTF8Encoding.UTF8.GetString(buf, 0, read);
+                        lock(Msgs)
+                        {
+                        Msgs.Add(input);
+                        }
+                    }
+                    // write
+                    if (SendMessages.Count > 0)
+                    { 
+                        byte[] buff = UTF8Encoding.UTF8.GetBytes( SendMessages[0] );
+                        lock(SendMessages){
+                        SendMessages.RemoveAt(0);
+                        }
+                        ns.Write(buff, 0, buff.Length);
+                    }
+                }
+                catch 
+                {
+                    Msgs.Add("disconnected ><");
+                    return;
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+    }
+}
---
+++ chatSrv/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+
+namespace chatSrv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Server srv = new Server();
+            srv.Run();           
+        }
+    }
+
+
+    class Server
+    {
+        int port = 12312;
+        TcpListener listener;
+        List<TcpClient> Clients = new List<TcpClient>();
+
+
+        public Server()
+        {
+            listener = new TcpListener( IPAddress.Any, port);
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(">> waiting for clients");
+            listener.Start();
+            while (true)
+            {
+               TcpClient client = listener.AcceptTcpClient();
+               Clients.Add(client);
+               Thread clientThread = new Thread( new ParameterizedThreadStart( ClientThreadBody ) );
+               clientThread.Start(client);
+               Console.WriteLine(">> client connected");
+            }
+        }
+
+        void ClientThreadBody(object Client)
+        {
+            TcpClient CurrentClient = (TcpClient)Client;
+            NetworkStream stream = CurrentClient.GetStream();
+
+            SendToAll("Кто-то приконнектился! " + CurrentClient.Client.RemoteEndPoint.ToString());
+
+            while(CurrentClient.Connected)
+            {
+                try {
+                    if (stream.DataAvailable)
+                    {
+                        byte[] buff = new byte[4096];
+                        int read = stream.Read(buff, 0, 4096);
+                        string input = UTF8Encoding.UTF8.GetString(buff,0, read);
+                        Console.WriteLine( CurrentClient.Client.RemoteEndPoint.ToString() + ": " + input);
+
+                        SendToAll(CurrentClient.Client.RemoteEndPoint.ToString() + ": " + input);
+                    }
+                }
+                catch {                    
+                    return;
+                }
+                /***/
+                Thread.Sleep(1);
+            }
+            
+        }
+
+        public void SendToAll(string data)
+        {
+            byte[] buffer;
+            buffer = UTF8Encoding.UTF8.GetBytes(data);
+            foreach (TcpClient client in Clients)
+            { 
+                try
+                {
+                    client.GetStream().Write(buffer, 0, buffer.Length);
+                }
+                catch
+                {
+                    lock (Clients) { 
+                        Clients.Remove(client);                         
+                    }
+                }
+            }
+        }
+
+    }
+}
